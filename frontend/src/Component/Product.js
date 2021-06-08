0586d5ac2a52810e6671ffea83ae541fee7f4e7d
Shefali Gupta <shefalisg009@gmail.com>
2021-06-08 18:41:51 +0530

    insert header,footer,rating and images

---
+++ frontend/src/Component/Product.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+
+const Product = ( { product } ) => {
+    return (
+        <Card className='my-5 p-3 rounded'>
+            <a href ={`/product/${product._id}`}>
+             <Card.Img src = {product.image} varient ='top' />
+            </a>
+
+            <Card.Body>
+                 <a href ={`/product/${product._id}`}>
+                   <Card.Title as='div'>
+                   <strong>{product.name}</strong>
+                   </Card.Title>
+                 </a>
+            </Card.Body>
+
+            <Card.Text as = 'div'>
+              <Rating value = {product.rating} 
+              text = {`${product.numReviews} reviews`}
+             />
+            </Card.Text>
+          
+          <Card.Text as = 'h3'>
+              ${product.price}
+          </Card.Text>
+        </Card>
+
+        
+    )
+}
+
+export default Product
